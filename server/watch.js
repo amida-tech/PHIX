7517b962671d98ce08c56bd962102f86b0a45733
Matthew McCall <matt@mattmccall.com>
2014-02-03 09:59:41 -0800

    Remove clinician from watch.js.

--- server/watch.js
+++ server/watch.js
@@ -16,45 +16,29 @@ limitations under the License.
 
 //Watch inbox dir and add messages to PHIX database
 var Account = require('./models/account'),
-    Personal = require('./models/personal'),
-    Message = require('./models/message'),
-    fs = require('fs'),
+	Personal = require('./models/personal'),
+	Message = require('./models/message'),
+	fs = require('fs'),
 	blueButton = require('./lib/parse/bluebutton.js'),
 
-    mongoose = require('mongoose'),
-    mongo = require('mongodb'),
-    ObjectId = require('mongodb').ObjectID,
-    Db = mongo.Db,
-    Grid = mongo.Grid,
+	mongoose = require('mongoose'),
+	mongo = require('mongodb'),
+	ObjectId = require('mongodb').ObjectID,
+	Db = mongo.Db,
+	Grid = mongo.Grid,
 
-    archiver = require('archiver');
+	archiver = require('archiver');
 
 var simplesmptp = require('simplesmtp');
 
 var MailComposer = require("mailcomposer").MailComposer;
 
-var host="";
-var receiver=""
-var db_name=""
-
-// for phix app
-if ('phix' == process.env.NODE_ENV) {
-    host="hub.amida-demo.com";
-    receiver="node.amida-demo.com";
-
-    db_name="portal";
-}
-
-// for clinician app
-if ('clinician' == process.env.NODE_ENV) {
-    host="node.amida-demo.com";
-    receiver="hub.amida-demo.com";
-
-    db_name="portal_clinician";
-}
+var host = "hub.amida-demo.com";
+var receiver = "node.amida-demo.com";
+var db_name = "portal";
 
 
-var areWeDone=3*2+3*3+3+3+3+3;
+var areWeDone = 3 * 2 + 3 * 3 + 3 + 3 + 3 + 3;
 
 var grid;
 var db;
@@ -62,190 +46,206 @@ var db;
 dbConn();
 
 function dbConn() {
-  Db.connect('mongodb://localhost/'+db_name, function(err, dbase) {
-    if (err) throw err;
-    db = dbase;
-    grid = new Grid(db, 'storage');
-    doit();
-  });
+	Db.connect('mongodb://localhost/' + db_name, function(err, dbase) {
+		if (err) throw err;
+		db = dbase;
+		grid = new Grid(db, 'storage');
+		doit();
+	});
 }
 
-function doit(){
-var fs = require('fs'),
-    Message = require('./models/message'),
-    ncp = require('ncp').ncp;
+function doit() {
+	var fs = require('fs'),
+		Message = require('./models/message'),
+		ncp = require('ncp').ncp;
 
-var MailParser = require("mailparser").MailParser;
+	var MailParser = require("mailparser").MailParser;
 
-var files_root="./";
+	var files_root = "./";
 
-// Connect mongoose
-//mongoose.connect('mongodb://localhost/portal-test');
-mongoose.connect('mongodb://localhost/'+db_name);
-
-
-var chokidar = require('chokidar');
-
-//TODO: change to production dir
-//var watch_dir="/opt/direct/incoming";
-
-var watch_dir="eml";
-
-var watcher = chokidar.watch('file or dir', {ignored: /^\./, persistent: true});
-
-watcher
-  .on('add', function(path) {console.log('File', path, 'has been added');})
-  .on('change', function(path) {console.log('File', path, 'has been changed');})
-
-// 'add' and 'change' events also receive stat() results as second argument.
-// http://nodejs.org/api/fs.html#fs_class_fs_stats
-watcher.on('add', function(path, stats) {
-  console.log('File', path, 'changed size to', stats.size);
-
-  process_file(path);
-  console.log("done processing");
-});
-
-watcher.add(watch_dir);
-
-// Only needed if watching is persistent.
-watcher.close();
+	// Connect mongoose
+	//mongoose.connect('mongodb://localhost/portal-test');
+	mongoose.connect('mongodb://localhost/' + db_name);
 
 
-function process_file(filepath) {
-		console.log("processing "+filepath);
+	var chokidar = require('chokidar');
+
+	//TODO: change to production dir
+	//var watch_dir="/opt/direct/incoming";
+
+	var watch_dir = "eml";
+
+	var watcher = chokidar.watch('file or dir', {
+		ignored: /^\./,
+		persistent: true
+	});
+
+	watcher
+		.on('add', function(path) {
+			console.log('File', path, 'has been added');
+		})
+		.on('change', function(path) {
+			console.log('File', path, 'has been changed');
+		})
+
+	// 'add' and 'change' events also receive stat() results as second argument.
+	// http://nodejs.org/api/fs.html#fs_class_fs_stats
+	watcher.on('add', function(path, stats) {
+		console.log('File', path, 'changed size to', stats.size);
+
+		process_file(path);
+		console.log("done processing");
+	});
+
+	watcher.add(watch_dir);
+
+	// Only needed if watching is persistent.
+	watcher.close();
+
+
+	function process_file(filepath) {
+		console.log("processing " + filepath);
 
 		var mailparser = new MailParser();
 
-		var att_count=0;
+		var att_count = 0;
 
-		mailparser.on("end", function(mail_object){
+		mailparser.on("end", function(mail_object) {
 
-		    console.log("Subject:", mail_object.subject);
-		    console.log("From:", mail_object.from);
-		    console.log("To:", mail_object.to);
+			console.log("Subject:", mail_object.subject);
+			console.log("From:", mail_object.from);
+			console.log("To:", mail_object.to);
 			console.log("Text body:", mail_object.text);
 			console.log("Attachments:", mail_object.attachments);
 
-			var username=mail_object.to[0].address.substring(0,mail_object.to[0].address.search("@"));
-			console.log('username= '+username);
+			var username = mail_object.to[0].address.substring(0, mail_object.to[0].address.search("@"));
+			console.log('username= ' + username);
 			//username="janedoe";
 
-    		var messageJSON = {};
-	        messageJSON.sender = mail_object.from[0].address;
-            messageJSON.recipient = mail_object.to[0].address;
-            messageJSON.received = new Date();
-            messageJSON.subject = mail_object.subject;
-            messageJSON.contents = mail_object.text;
-            messageJSON.read = false;
-            messageJSON.archived = false;
-            messageJSON.attachments = []; //requestJSON.attachments;
+			var messageJSON = {};
+			messageJSON.sender = mail_object.from[0].address;
+			messageJSON.recipient = mail_object.to[0].address;
+			messageJSON.received = new Date();
+			messageJSON.subject = mail_object.subject;
+			messageJSON.contents = mail_object.text;
+			messageJSON.read = false;
+			messageJSON.archived = false;
+			messageJSON.attachments = []; //requestJSON.attachments;
 
-            to=messageJSON.recipient.split("@")[0]+"@"+receiver;
-            from=messageJSON.sender.split("@")[0]+"@"+host;
+			to = messageJSON.recipient.split("@")[0] + "@" + receiver;
+			from = messageJSON.sender.split("@")[0] + "@" + host;
 
-	        messageJSON.sender = from;
-            messageJSON.recipient = to;
-            //messageJSON.recipient = "janedoe@hub.amida-demo.com";
+			messageJSON.sender = from;
+			messageJSON.recipient = to;
+			//messageJSON.recipient = "janedoe@hub.amida-demo.com";
 
-            console.log("to: "+to+", from: "+from);
+			console.log("to: " + to + ", from: " + from);
 
-			att_count=0;
-			if (mail_object.attachments) att_count=mail_object.attachments.length;
+			att_count = 0;
+			if (mail_object.attachments) att_count = mail_object.attachments.length;
 
-            if (att_count>0){
-            	console.log("we have attachments");
-			for (a in mail_object.attachments){
+			if (att_count > 0) {
+				console.log("we have attachments");
+				for (a in mail_object.attachments) {
 
 
-				var attachment=mail_object.attachments[a];
-				console.log("generatedFileName "+attachment.generatedFileName);
-				//fs.writeFileSync(files_root+"testFiles/"+attachment.generatedFileName, attachment.content);
+					var attachment = mail_object.attachments[a];
+					console.log("generatedFileName " + attachment.generatedFileName);
+					//fs.writeFileSync(files_root+"testFiles/"+attachment.generatedFileName, attachment.content);
 
-				//TODO: save attachment to mongo
+					//TODO: save attachment to mongo
 
-				console.log("attachment saved");
-				var filename=attachment.generatedFileName;
-				var content = attachment.content;
+					console.log("attachment saved");
+					var filename = attachment.generatedFileName;
+					var content = attachment.content;
 
-				//content is a Buffer
-				//console.log(content);
+					//content is a Buffer
+					//console.log(content);
 
-				content=content.toString()
+					content = content.toString()
 
-				//console.log(content);
+					//console.log(content);
 
-				var fileType="binary/octet";
+					var fileType = "binary/octet";
 
-				  try {
-				    var bb = blueButton(content);
+					try {
+						var bb = blueButton(content);
 
-				    var bbMeta = bb.document();
+						var bbMeta = bb.document();
 
-				    if (bbMeta.type === 'ccda') {
-				      fileType="CCDA";
-				    }
-				  }
-				  catch(e)
-				  {
-				    //do nothing, keep original fileType
-				  }
+						if (bbMeta.type === 'ccda') {
+							fileType = "CCDA";
+						}
+					} catch (e) {
+						//do nothing, keep original fileType
+					}
 
-				  console.log("fileType: "+fileType);
+					console.log("fileType: " + fileType);
 
 					//Save attacheent to storage
-				  var buffer = new Buffer(content);
-				  grid.put(buffer, {metadata: {source: "inbox", details: mail_object.subject,owner: username, parsedFlag: false}, 'filename': filename, 'content_type': fileType}, function(err, fileInfo) {
-				    if(err) throw err;
-				    var recordId = fileInfo._id
-				    //console.log("Record Stored in Gridfs: " + recordId);
-				    //console.log(fileInfo);
+					var buffer = new Buffer(content);
+					grid.put(buffer, {
+						metadata: {
+							source: "inbox",
+							details: mail_object.subject,
+							owner: username,
+							parsedFlag: false
+						},
+						'filename': filename,
+						'content_type': fileType
+					}, function(err, fileInfo) {
+						if (err) throw err;
+						var recordId = fileInfo._id
+						//console.log("Record Stored in Gridfs: " + recordId);
+						//console.log(fileInfo);
 
-				    identifier=fileInfo._id;
+						identifier = fileInfo._id;
 
-				    //res.send({fileName: fileInfo.filename, identifier: fileInfo._id});
-				    messageJSON.attachments.push({fileName: filename, identifier: identifier});
-				    done();
-				  });
+						//res.send({fileName: fileInfo.filename, identifier: fileInfo._id});
+						messageJSON.attachments.push({
+							fileName: filename,
+							identifier: identifier
+						});
+						done();
+					});
+				}
+			} else {
+				console.log("no attachments");
+				var inputMessage = new Message(messageJSON);
+				inputMessage.save(function(err, res) {
+					if (err) throw err;
+					//Send the message out.
+					//callback();
+				});
+
 			}
-		}
-		else{
-			console.log("no attachments");
-            var inputMessage = new Message(messageJSON);
-            inputMessage.save(function(err, res) {
-               if (err) throw err;
-               //Send the message out.
-                //callback();
-            });
 
-		}
+			var count = 0;
 
-            var count = 0;
+			function done() {
+				count = count + 1;
+				if (count == att_count) {
+					console.log("saving message with all attachments");
+					var inputMessage = new Message(messageJSON);
+					inputMessage.save(function(err, res) {
+						if (err) throw err;
+						//Send the message out.
+						//callback();
+					});
 
-            function done() {
-                count = count + 1;
-                if (count == att_count) {
-                    console.log("saving message with all attachments");
-                    var inputMessage = new Message(messageJSON);
-                    inputMessage.save(function(err, res) {
-                        if (err) throw err;
-                        //Send the message out.
-                        //callback();
-                    });
+				}
 
-                }
-
-            }
+			}
 
 		});
 
 		//fs.createReadStream("test/email.eml").pipe(mailparser);
 		fs.createReadStream(filepath).pipe(mailparser);
 
-		function done(){
-			        console.log("done");
+		function done() {
+			console.log("done");
 		}
 
 
-};
+	};
 };
